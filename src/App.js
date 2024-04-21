a46e0dd0393de799556b81c3785446c2670f61a4
unknown <bhargavi.singh@criodo.com>
2024-04-21 17:22:56 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks"
 
 export const config = {
-  endpoint: `https://qkart-frontend-xi0i.onrender.com/api/v1`,
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
 };
 
 function App() {
